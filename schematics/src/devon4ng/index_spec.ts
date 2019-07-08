00341746a7fe381d4b34bc6f4acd67064c76fe0c
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2019-07-08 10:58:00 +0200

    Updated travis ci

--- schematics/src/devon4ng/index_spec.ts
+++ schematics/src/devon4ng/index_spec.ts
@@ -1,5 +1,3 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
